0fff3b40b1f1c47d59d810dfa1e8edcd44556fda
김중현 <110381888+gotkagovkfl@users.noreply.github.com>
2024-11-10 03:30:55 +0900

    [기능추가] 적 공격 AI 추가 / 1웨이브는 적1마리만 나오도록

--- Assets/3. Scripts/Main/1_Tower/Tower.cs
+++ Assets/3. Scripts/Main/1_Tower/Tower.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 
 public class Tower : DestroyableSingleton<Tower>
 {
 
 
-    public int hp;
-    public int maxHp = 1000;
+    public float hp;
+    public float maxHp = 1000;
 
 
     public void Init()
     {
         hp = maxHp;
     }
+
+
+    public void GetDamaged(float dmg)
+    {
+        hp -= dmg;
+
+        if (hp<= 0)
+        {
+            DestroyTower();
+        }
+    }
+
+
+    public void DestroyTower()
+    {
+        // Destroy(gameObject);
+
+        Debug.LogError("패배!!!!!!!!!!");
+    }   
 
 }
